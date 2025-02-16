4c47dc20727927f69d6d8200a7342e469a6acee6
KODURISAIKIRAN <kodurisaikiran1437@gmail.com>
2025-02-16 12:31:25 +0530

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,48 @@
+import random
+rock = """   
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+paper = """
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+"""
+scissor = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+
+"""
+gestures = [rock,paper,scissor]
+print("              ____ROCK-PAPER-SCISSOR GAME____")
+print()
+print("Enter your choice for Rock:0,for paper:1,for scissor:2")
+user = int(input())
+print(gestures[user])
+comp = random.randint(0,2)
+print("Computer choosed:")
+print(gestures[comp])
+if user<3:
+    if user == comp:
+        print("Its Draw!")
+    elif user == 2 and comp == 0 :
+        print("You Lose :(")
+    elif comp == 2 and user == 0 :
+        print("You Win! :)")
+    elif user < comp:
+        print("You Lose :(")
+    elif comp < user:
+        print("You Win! :)")
+else :
+    print("User choice is Invalid You Lose :(")
